b5604533dd23442193bbf797c1076809d106263c
alexey <myboxfm@gmail.com>
2021-05-05 10:20:43 +0300

    before changes with question class updates

--- Tkinter_project/text.py
+++ Tkinter_project/text.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 
 class ScrolledFrame(tk.Frame):
@@ -23,7 +24,7 @@ class ScrolledFrame(tk.Frame):
         self._canvas.configure(xscrollcommand=self._horizontal_bar.set)
 
         # inner frame for widgets
-        self.inner = tk.Frame(self._canvas, bg='red')
+        self.inner = tk.Frame(self._canvas, bg='grey')
         self._window = self._canvas.create_window((0, 0), window=self.inner, anchor='nw')
 
         # autoresize inner frame
@@ -48,26 +49,67 @@ class Question:
         self.parent = parent
         self.question = question
         self.question_key = question_key
-        self.create_widgets_inside()
-        #self.answer=self.get_input()
-
-    def create_widgets_inside(self):
         self.labelframe = tk.LabelFrame(self.parent, text=self.question_key)
-        self.labelframe.pack(fill="both", expand=True)
-        # self.labelframe.pack(fill="both", expand=True)
+        self.var = StringVar(self.labelframe)
+        self.var.set('0')
 
-        self.label = tk.Label(self.labelframe, text=self.question,anchor='nw',justify='left')
-        self.label.pack(expand=True, fill='both',side='left')
+        # self.list_of_choices = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        # self.OptionMenu = tk.OptionMenu(self.labelframe, self.var, *self.list_of_choices, command=self.print_it)
 
-        self.entry = tk.Entry(self.labelframe,width='5',bd='3')
-        self.entry.pack(side='right',padx='10')
+    def print_it(self,event):
+        print(self.var.get())
 
+
+    def create_widgets_inside(self, counter):
+        if counter==0:
+            self.labelframe = tk.LabelFrame(self.parent, text=self.question_key)
+            self.labelframe.config(font=("Courier", 16),bg='grey')
+            self.labelframe.pack(fill="both", expand=True)
+
+            self.label = tk.Label(self.labelframe, text=self.question,anchor='nw',justify='left')
+            self.label.configure(bg='grey')
+            self.label.pack(expand=True, fill='both',side='left')
+
+
+            self.entry = tk.Entry(self.labelframe,width='20',bd='1')
+            self.entry.configure(bd='0')
+            self.entry.pack(side='right',padx='10',pady='15')
+
+        elif counter==1:
+            self.labelframe = tk.LabelFrame(self.parent, text=self.question_key)
+            self.labelframe.config(font=("Courier", 16),bg='grey')
+            self.labelframe.pack(fill="both", expand=True)
+
+            self.label = tk.Label(self.labelframe, text=self.question, anchor='nw', justify='left')
+            self.label.configure(bg='grey')
+            self.label.pack(expand=True, fill='both', side='left')
+            self.list_of_choices = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+            self.OptionMenu = tk.OptionMenu(self.labelframe, self.var, *self.list_of_choices, command=self.print_it)
+            self.OptionMenu.configure(width='5', bg='grey')
+            self.OptionMenu.pack(side='right')
+        else:
+            self.labelframe = tk.LabelFrame(self.parent, text=self.question_key)
+            self.labelframe.config(font=("Courier", 16),bg='grey')
+            self.labelframe.pack(fill="both", expand=True)
+
+            self.label = tk.Label(self.labelframe, text=self.question, anchor='nw', justify='left')
+            self.label.configure(bg='grey')
+            self.label.pack(expand=True, fill='both', side='left')
+
+            self.variable = StringVar(self.labelframe)
+            self.variable.set('0')  # this is a default value
+            self.entry = tk.Entry(root)#(self.labelframe, self.variable, '1','2', '3', '4', '5')
+            self.OptionMenu.configure(width='5',bg='grey')
+            self.OptionMenu.pack(side='right')
+    def get_drop_down_value(self,value):
+        return value
 
 # --- main ---
 
 root = tk.Tk()
 root.title("Seniority")
 root.geometry("600x600")
+root.config(bg='grey')
 questions = {
     "User_Name":"",
     "experience (years)": "",
@@ -114,24 +156,29 @@ questions = {
 
     "Team Management": "\n* pro-activity\n* test strategy (senior/manager)\n* metrics for QA/team (senior/manager)\n* visability (senior/manager)\n* solving problems (senior/manager)\n* delegation (senior/manager)\n* optimization of processes (senior/manager)"
 }
-def get_input(*args):
+def get_input(entry_values):
     pass
-    #value = self.entry.get()
-    #if value:
-    #    return value
-    # print('value:', value)
-    # if value == self.answer:
-    #     print("Esatto. è " + self.answer)
-    #     self.label['text'] = "Esatto"®
-button = tk.Button(root, text="Calculate", command=get_input,bg='red',width='15')
-button.config
-button.pack(side='bottom',anchor='e', padx='18')
+
 list_of_questions_keys = list(questions)
 list_of_questions_values = list(questions.values())
 window = ScrolledFrame(root)
 window.pack(expand=True, fill='both')
+button = tk.Button(root, text="Calculate", command=get_input,width='15',relief='sunken',bg='red' ,fg='yellow')
+button.pack(side='bottom',anchor='e', padx='18')
 counter=0
+entry_values=[]
 for i in range(len(list_of_questions_keys)):
-    Question(window.inner, list_of_questions_values[counter],list_of_questions_keys[counter])
+    item = Question(window.inner, list_of_questions_values[counter],list_of_questions_keys[counter])
+    item.create_widgets_inside(counter)
+    option_values=item.OptionMenu
+    print(option_values)
+    entry_values.append(option_values)
+    # w_i=item.OptionMenu.winfo_id()
+    # entry_values.append(w_i)
+    # entry_values.append(item.entry.winfo_id())
+    print(entry_values)
     counter+=1
+print(entry_values)
+# for entry in entry_values:
+#     entry.
 root.mainloop()
